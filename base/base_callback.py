7cd18e6c56594100e3f0a50a19ddbddf44ed958f
Zhaoze Wang <100132924+Wangzhaoze@users.noreply.github.com>
2023-08-23 23:00:38 +0200

    base callback

---
+++ base/base_callback.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2023-08-20
+# @Author  : Zhaoze Wang
+# @Site    : https://github.com/Wangzhaoze/BFNPL
+# @File    : base_callback.py
+# @IDE     : vscode
+
+"""
+Implement Base Callback
+"""
+
+import pytorch_lightning as pl
+
+class BaseCallback(pl.Callback):
+    """
+    Base class for custom callbacks in PyTorch Lightning.
+
+    This class provides hooks that can be overridden in derived classes to customize
+    the behavior during different phases of the training process.
+
+    Attributes:
+        None
+        
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes a new instance of the BaseCallback class.
+        """
+        super().__init__()
+
+    def on_validation_epoch_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        """
+        Called when a validation epoch begins.
+
+        Args:
+            trainer (pl.Trainer): The PyTorch Lightning trainer object.
+            pl_module (pl.LightningModule): The PyTorch Lightning module being trained.
+
+        Returns:
+            None
+        """
+
+    def on_validation_epoch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        """
+        Called when a validation epoch ends.
+
+        Args:
+            trainer (pl.Trainer): The PyTorch Lightning trainer object.
+            pl_module (pl.LightningModule): The PyTorch Lightning module being trained.
+
+        Returns:
+            None
+        """
+
+    def on_validation_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        """
+        Called when the validation loop begins.
+
+        Args:
+            trainer (pl.Trainer): The PyTorch Lightning trainer object.
+            pl_module (pl.LightningModule): The PyTorch Lightning module being trained.
+
+        Returns:
+            None
+        """
+
+    def on_validation_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        """
+        Called when the validation loop ends.
+
+        Args:
+            trainer (pl.Trainer): The PyTorch Lightning trainer object.
+            pl_module (pl.LightningModule): The PyTorch Lightning module being trained.
+
+        Returns:
+            None
+        """
