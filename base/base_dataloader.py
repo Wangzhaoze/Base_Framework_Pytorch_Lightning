ae5e744e97163f07d9ab427ff630b267f2e4f974
Zhaoze Wang <100132924+Wangzhaoze@users.noreply.github.com>
2023-08-24 22:03:03 +0200

    base dataloader

--- base/base_dataloader.py
+++ base/base_dataloader.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2023-08-20
 # @Author  : Zhaoze Wang
-# @Site    : https://github.com/Wangzhaoze/BFNPL
+# @Site    : https://github.com/Wangzhaoze/BNFPL
 # @File    : base_dataloader.py
 # @IDE     : vscode
 
