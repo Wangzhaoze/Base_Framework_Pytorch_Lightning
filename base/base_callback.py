6dfebca0e0f48d6e231d3c92c0b7ded2f219c0a9
wzz <wangzhaoze@outlook.com>
2023-11-28 21:31:16 +0100

    base callback

--- base/base_callback.py
+++ base/base_callback.py
@@ -30,7 +30,7 @@ class BaseCallback(pl.Callback):
         """
         super().__init__()
 
-    def on_validation_epoch_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+    def on_validation_epoch_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
         """
         Called when a validation epoch begins.
 
@@ -42,7 +42,7 @@ class BaseCallback(pl.Callback):
             None
         """
 
-    def on_validation_epoch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+    def on_validation_epoch_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
         """
         Called when a validation epoch ends.
 
@@ -54,7 +54,7 @@ class BaseCallback(pl.Callback):
             None
         """
 
-    def on_validation_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+    def on_validation_start(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
         """
         Called when the validation loop begins.
 
@@ -66,7 +66,7 @@ class BaseCallback(pl.Callback):
             None
         """
 
-    def on_validation_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+    def on_validation_end(self, trainer: pl.Trainer, pl_module: pl.LightningModule) -> None:
         """
         Called when the validation loop ends.
 
